4bef757e558cdcee218e01bfe60438fa3bf8fd59
Chris Everhart <chris.everhart@furman.edu>
2026-03-31 16:06:46 -0400

    update missing sections to use variables from the file.

--- module03/create-env.sh
+++ module03/create-env.sh
@@ -36,25 +36,47 @@ echo $SUBNET2B
 
 echo 'Creating the TARGET GROUP and storing the ARN in $TARGETARN...'
 # https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/create-target-group.html
-TARGETARN=
+TARGETARN=$(aws elbv2 create-target-group \
+  --name $8 \
+  --protocol HTTP \
+  --port 80 \
+  --vpc-id $VPCID \
+  --query "TargetGroups[*].TargetGroupArn" \
+  --output=text)
 
 echo "Creating ELBv2 Elastic Load Balancer..."
 #https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/create-load-balancer.html
-ELBARN=
+ELBARN=$(aws elbv2 create-load-balancer \
+  --name $9 \
+  --subnets $SUBNET2A $SUBNET2B \
+  --security-groups $4 \
+  --query "LoadBalancers[*].LoadBalancerArn" \
+  --output=text)
 echo $ELBARN
 
 # AWS elbv2 wait for load-balancer available
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/wait/load-balancer-available.html
 echo "Waiting for load balancer to be available..."
-aws elbv2 wait load-balancer-available 
+aws elbv2 wait load-balancer-available --load-balancer-arns $ELBARN
 echo "Load balancer available..."
 # create AWS elbv2 listener for HTTP on port 80
 #https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/create-listener.html
-aws elbv2 create-listener 
+aws elbv2 create-listener \
+  --load-balancer-arn $ELBARN \
+  --protocol HTTP \
+  --port 80 \
+  --default-actions Type=forward,TargetGroupArn=$TARGETARN
 
 echo "Beginning to create and launch instances..."
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/run-instances.html
-aws ec2 run-instances 
+aws ec2 run-instances \
+  --image-id $1 \
+  --instance-type $2 \
+  --key-name $3 \
+  --security-group-ids $4 \
+  --count $5 \
+  --user-data file://$6 \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$7}]"
 
 # Collect Instance IDs
 # https://stackoverflow.com/questions/31744316/aws-cli-filter-or-logic
@@ -82,7 +104,10 @@ fi
 
 # Retreive ELBv2 URL via aws elbv2 describe-load-balancers --query and print it to the screen
 #https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/describe-load-balancers.html
-URL=$(aws elbv2 describe-load-balancers)
+URL=$(aws elbv2 describe-load-balancers \
+  --load-balancer-arns $ELBARN \
+  --query "LoadBalancers[*].DNSName" \
+  --output=text)
 echo $URL
 
 # end of outer fi - based on arguments.txt content
