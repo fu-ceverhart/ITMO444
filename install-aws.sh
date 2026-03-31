bc146630ba7bef51a6cbfa0e63fb88b1e6ddfdda
Chris Everhart <chris.everhart@furman.edu>
2026-03-31 15:31:03 -0400

    init commit for script to no root install aws-cli

---
+++ install-aws.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install --install-dir ~/aws-cli --bin-dir ~/bin
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
+aws --version
