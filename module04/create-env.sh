ca4196e25b4a6ea5b51a3156711966b3fdfd186b
Chris Everhart <chris.everhart@furman.edu>
2026-04-07 14:05:29 -0400

    update url

--- module04/create-env.sh
+++ module04/create-env.sh
@@ -124,7 +124,10 @@ fi
 
 # Retreive ELBv2 URL via aws elbv2 describe-load-balancers --query and print it to the screen
 #https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/describe-load-balancers.html
-URL=
+URL=$(aws elbv2 describe-load-balancers \
+  --load-balancer-arns $ELBARN \
+  --query "LoadBalancers[*].DNSName" \
+  --output=text)
 echo $URL
 
 # end of outer fi - based on arguments.txt content
