0bcda424f16db277829f429a731392b48676108d
Chris Everhart <chris.everhart@furman.edu>
2026-04-07 14:04:59 -0400

    update target groups

--- module04/create-env.sh
+++ module04/create-env.sh
@@ -65,7 +65,13 @@ LAUNCHTEMPLATEID=
 
 echo 'Creating the TARGET GROUP and storing the ARN in $TARGETARN'
 # https://awscli.amazonaws.com/v2/documentation/api/2.0.34/reference/elbv2/create-target-group.html
-TARGETARN=
+TARGETARN=$(aws elbv2 create-target-group \
+  --name $8 \
+  --protocol HTTP \
+  --port 80 \
+  --vpc-id $VPCID \
+  --query "TargetGroups[*].TargetGroupArn" \
+  --output=text)
 echo $TARGETARN
 
 echo "Creating ELBv2 Elastic Load Balancer..."
