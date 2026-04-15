6f9b683833f40138c2b8534d918c5f9136d91026
Chris Everhart <chris.everhart@intele-gents.com>
2026-04-14 21:05:59 -0400

    move items over from module4 file to module5 file

--- module05/create-env.sh
+++ module05/create-env.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 ##############################################################################
-# Module-04
+# Module-05
 # This assignment requires you to modify your previous scripts and use the 
 # Launch Template and Autoscaling group commands for creating EC2 instances
 # You will need an additional script to generate a JSON file with parameters
-# for your launch template
+# for your launch template. You will need to add an extras storage harddisk (EBS)
+# to each EC2 instance, define and IAM profile to use and define the name of two
+# S3 buckets to create
 # 
 # You will need to define these variables in a txt file named: arguments.txt
 # 1 image-id
@@ -24,17 +26,20 @@
 # 15 ASG max
 # 16 ASG desired
 # 17 AWS Region for LaunchTemplate (use your default region)
+# 18 EBS disk storage size in GB
+# 19 S3 Bucket One
+# 20 S3 Bucket Two
 ##############################################################################
 export AWS_PAGER=""
 ltconfigfile="./config.json"
 
 if [ $# = 0 ]
 then
-  echo 'You do not have enough variable in your arugments.txt, perhaps you forgot to run: bash ./create-env.sh $(< ~/arguments.txt)'
+  echo "You don't have enough variables in your arugments.txt, perhaps you forgot to run: bash ./create-env.sh \$(< ~/arguments.txt)"
   exit 1 
 elif ! [[ -a $ltconfigfile ]]
   then
-   echo 'The launch template configuration JSON file does not exist - make sure you run/ran the command: bash ./create-lt-json.sh $(< ~/arguments.txt) command before running the create-env.sh $(< ~/arguments.txt)'
+  echo "The launch template configuration JSON file doesn't exist - make sure you run/ran the command: bash ./create-lt-json.sh \$(< ~/arguments.txt) command before running the create-env.sh \$(< ~/arguments.txt)"
    echo "Now exiting the program..."
    exit 1
 # else run the creation logic
@@ -98,15 +103,11 @@ aws elbv2 modify-target-group-attributes --target-group-arn $TARGETARN --attribu
 # AWS elbv2 wait for load-balancer available
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/wait/load-balancer-available.html
 echo "Waiting for load balancer to be available..."
-aws elbv2 wait load-balancer-available
+aws elbv2 wait load-balancer-available --load-balancer-arns $ELBARN
 echo "Load balancer available..."
 # create AWS elbv2 listener for HTTP on port 80
 #https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/create-listener.html
-aws elbv2 create-listener \
-    --load-balancer-arn $ELBARN \
-    --protocol HTTP \
-    --port 80 \
-    --default-actions Type=forward,TargetGroupArn=$TARGETARN
+aws elbv2 create-listener --load-balancer-arn $ELBARN --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=$TARGETARN
 
 echo 'Creating Auto Scaling Group...'
 # Create Autoscaling group ASG - needs to come after Target Group is created
@@ -140,6 +141,41 @@ if [ "$INSTANCEIDS" != "" ]
     echo 'There are no running or pending values in $INSTANCEIDS to wait for...'
 fi 
 
+# Add S3api commands to create two S3 buckets
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/s3api/index.html
+echo "Creating S3 bucket: ${19}..."
+aws s3api create-bucket 
+echo "Created S3 bucket: ${19}..."
+
+echo "Creating S3 bucket: ${20}..."
+aws s3api create-bucket 
+echo "Created S3 bucket: ${20}..."
+
+# S3 commands
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/s3/index.html
+# Upload illinoistech.png and rohit.jpg to bucket ${19}
+echo "Uploading image: ./images/illinoistech.png to s3://${19}..."
+aws s3 cp 
+echo "Uploaded image: ./images/illinoistech.png to s3://${19}..."
+
+echo "Uploading image: ./images/rohit.jpg to s3://${19}..."
+aws s3 cp 
+echo "Uploaded image: ./images/rohit.jpg to s3://${19}..."
+
+echo "Listing content of bucket: s3://${19}..."
+aws s3 ls 
+
+# Upload ranking.jpg and elevate.webp to bucket ${20}
+echo "Uploading image: ./images/elevate.webp to s3://${20}..."
+aws s3 cp 
+echo "Uploaded image: ./images/elevate.webp to s3://${20}..."
+
+echo "Uploading image: ./images/ranking.jpg to s3://${20}..."
+aws s3 cp 
+echo "Uploaded image: ./images/ranking.jpg to s3://${20}..."
+
+echo "Listing content of bucket: s3://${20}..."
+aws s3 ls 
 
 # Retreive ELBv2 URL via aws elbv2 describe-load-balancers --query and print it to the screen
 #https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/describe-load-balancers.html
