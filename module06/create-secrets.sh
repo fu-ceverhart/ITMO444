89c2fe42801551b537c245a9178463a39363d041
Chris Everhart <chris.everhart@intele-gents.com>
2026-04-21 16:11:39 -0400

    fix create secrets to remove SED and just use jq.

--- module06/create-secrets.sh
+++ module06/create-secrets.sh
@@ -8,9 +8,9 @@ echo "Creating AWS secret..."
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/secretsmanager/get-secret-value.html#examples
 aws secretsmanager create-secret --name ${21} --secret-string file://maria.json
 
-SECRET_ID=$(aws secretsmanager list-secrets --filters Key=name,Values=${21} --query 'SecretList[*].ARN')
-USERVALUE=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID --output=json | jq '.SecretString' | sed 's/[\\nt]//g' | sed 's/^"//g' | sed 's/"$//g' | jq '.user' | sed 's/"//g')
-PASSVALUE=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID --output=json | jq '.SecretString' | sed 's/[\\nt]//g' | sed 's/^"//g' | sed 's/"$//g' | jq '.pass' | sed 's/"//g')
+SECRET_ID=$(aws secretsmanager list-secrets --filters Key=name,Values=${21} --query 'SecretList[0].ARN' --output text)
+USERVALUE=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID --output=json | jq -r '.SecretString | fromjson | .user')
+PASSVALUE=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID --output=json | jq -r '.SecretString | fromjson | .pass')
 
 echo $USERVALUE
 echo $PASSVALUE
