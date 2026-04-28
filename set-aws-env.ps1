8bd31f6eba9d60c527078c5880df418339270b9b
Chris Everhart <chris.everhart@furman.edu>
2026-04-28 13:50:52 -0400

    pull over from ITMO463 repo where i use to get my vars loaded for aws connectivity for terraform.

---
+++ set-aws-env.ps1
@@ -0,0 +1,15 @@
+# Run with . .\set-aws-env.ps1
+
+$envFile = Join-Path $PSScriptRoot ".env"
+
+foreach ($line in Get-Content $envFile) {
+    $parts = $line.Split("=")
+    $key   = $parts[0]
+    $value = $parts[1]
+
+    if ($key -eq "ACCESS_KEY") { $env:AWS_ACCESS_KEY_ID     = $value }
+    if ($key -eq "SECRET_KEY") { $env:AWS_SECRET_ACCESS_KEY = $value }
+    if ($key -eq "REGION")     { $env:AWS_DEFAULT_REGION    = $value }
+}
+
+Write-Host "AWS credentials set."
