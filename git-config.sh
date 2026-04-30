21092fd0cb863e1180902124eab758db183d36c2
fu-ceverhart <ceverhart@hawk.illinoistech.edu>
2026-04-30 16:45:48 -0400

    copy over requirements.txt

--- git-config.sh
+++ git-config.sh
@@ -1,4 +1,4 @@
-git config --global user.name "Chris Everhart"
+git config --global user.name "fu-ceverhart"
 git config --global user.email "ceverhart@hawk.illinoistech.edu"
 git config --global credential.helper store
 git push
